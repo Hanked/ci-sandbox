20f47ebb174b6c4f4b8f0f66cf557c8577af4ce2
Hanked <dave.kingsnorth@papershaker.com>
2015-01-26 12:09:21 +0000

    add assertion

--- test/lib.test.js
+++ test/lib.test.js
@@ -11,6 +11,7 @@ QUnit.test("isDivisibleBy cases", function(assert) {
 
     // call divide function for each test case
     divide(50, 10, true);
+    divide(50, 5, true);
     divide(99, 9, true);
     divide(20, 5, true);
     divide(56, 8, true);
